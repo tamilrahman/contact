c42b1418ed382ce5d69c5c299a200d11f15aa671
tamilrahman <tamilrahmanic@gmail.com>
2015-09-11 16:54:24 +0530

    add simple bootstrap ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.includes(:addresses).all
   end
 
   # GET /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :addresses
   accepts_nested_attributes_for :addresses, reject_if: :all_blank, allow_destroy: true
+
+  validates_presence_of :name, :email
 end
